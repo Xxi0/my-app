62f1ada5a4dca2003aa3787dc582fab9a072ba80
Xxi0 <Xxi0@users.noreply.github.com>
2015-03-22 21:30:38 -0600

    Agregue el formulario

--- app.py
+++ app.py
@@ -2,12 +2,26 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
+
+
+class IdeaForm(Form):
+    idea = StringField('idea', validators=[DataRequired()])
+    submit_button = SubmitField('Add idea')
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'devkey'
+
 Bootstrap(app)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    form = IdeaForm()
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
